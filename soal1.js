2b8c2dcee8951e1c5aa08a98cb2f087778ec9f2a
Rahayus09 <rahayustg09@gmail.com>
2022-03-14 13:32:47 +0700

    init MBKM Rahayu

---
+++ soal1.js
@@ -0,0 +1,8 @@
+ function changedWord(selectText, changeText, text) {
+    return text.split(selectText).join(changeText);
+}
+var kalimat1 = 'Andini sangat mencintai kamu selamanya';
+var kalimat2 = 'Gunung bromo tak akan mampu menggambarkan besarnya cintaku padamu';
+
+console.log(changedWord('mencintai', 'membenci', kalimat1));
+console.log(changedWord('bromo', 'semeru', kalimat2));
